99e1940faa8cde97c63446e4e88d84bbd9c24581
ian.aitken <ian.aitken@anaplan.com>
2022-11-14 10:41:50 +0000

    add fix for memory problems

--- grapher.py
+++ grapher.py
@@ -1,5 +1,33 @@
 import os
+import sys
+
 import pandas
+
+# Solving memory leak problem in pandas
+
+from ctypes import cdll, CDLL
+
+try:
+    cdll.LoadLibrary("libc.so.6")
+    libc = CDLL("libc.so.6")
+    libc.malloc_trim(0)
+except (OSError, AttributeError):
+    libc = None
+
+__old_del = getattr(pandas.DataFrame, '__del__', None)
+
+
+def __new_del(self):
+    if __old_del:
+        __old_del(self)
+    libc.malloc_trim(0)
+
+
+if libc:
+    print('Applying monkeypatch for pd.DataFrame.__del__', file=sys.stderr)
+    pandas.DataFrame.__del__ = __new_del
+else:
+    print('Skipping monkeypatch for pd.DataFrame.__del__: libc or malloc_trim() not found', file=sys.stderr)
 
 
 def plot(prices, times):
